72ec8ec89356d100af1f24c9a4995e91a2205bb6
rob <3358632+rjarratt@users.noreply.github.com>
2024-11-10 18:28:47 +0000

    Keep repeating the read of the URL

--- itty.c
+++ itty.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <curl/curl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int process(char *url);
-void read_data(CURLM *multi);
+void read_data(CURLM *multi, CURL *curl, char *url);
+size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp);
 
 int main(int argc,char *argv[])
 {
@@ -13,7 +18,7 @@ int main(int argc,char *argv[])
     }
     else
     {
-        result = process(argv[1]);
+           result = process(argv[1]);
     }
 
     return result;
@@ -21,8 +26,8 @@ int main(int argc,char *argv[])
 
 int process(char *url)
 {
-    CURL *curl;
     CURLM *multi;
+    CURL *curl;
     CURLcode res;
 
     curl_global_init(CURL_GLOBAL_DEFAULT);
@@ -32,39 +37,45 @@ int process(char *url)
         multi = curl_multi_init();
         if (multi)
         {
-            curl_easy_setopt(curl, CURLOPT_URL, url);
-            curl_multi_add_handle(multi, curl);
-
-            read_data(multi);
-
-            curl_multi_remove_handle(multi, curl);
-            curl_easy_cleanup(curl);
-            curl_multi_cleanup(curl);
-            curl_global_cleanup();
+            while (1)
+            {
+                read_data(multi, curl, url);
+            }
         }
         else
         {
             fprintf(stderr, "Cannot initialise CurlM\n");
         }
+
+        curl_multi_cleanup(curl);
     }
     else
     {
         fprintf(stderr, "Cannot initialise Curl\n");
     }
 
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+
     return 0;
 }
 
-void read_data(CURLM *multi)
+void read_data(CURLM *multi, CURL *curl, char *url)
 {
     int still_running = 1;
+
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+    curl_multi_add_handle(multi, curl);
+
     do
     {
         CURLMcode mc = curl_multi_perform(multi, &still_running);
- 
-        if(!mc)
-        /* wait for activity, timeout or "nothing" */
-        mc = curl_multi_poll(multi, NULL, 0, 1000, NULL);
+        if (!mc)
+        {
+            /* wait for activity, timeout or "nothing" */
+            mc = curl_multi_poll(multi, NULL, 0, 1000, NULL);
+        }
  
         if (mc)
         {
@@ -74,4 +85,13 @@ void read_data(CURLM *multi)
  
     }
     while(still_running);
+
+    curl_multi_remove_handle(multi, curl);
 }
+
+size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp)
+{
+    int result = fwrite(buffer, size, nmemb, stdout);
+    return result;
+}
+
